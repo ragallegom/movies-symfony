80ffaa5506f28cd46025f511277a3004d9c39cb7
RODRIGO GALLEGO <50471819+ragallegom@users.noreply.github.com>
2023-02-22 15:21:13 -0500

    add view in twig

--- src/Controller/MoviesController.php
+++ src/Controller/MoviesController.php
@@ -4,16 +4,16 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MoviesController extends AbstractController
 {
-    #[Route('/movies', name: 'app_movies')]
-    public function index(): JsonResponse
+    #[Route('/movies', name: 'movies')]
+    public function index(): Response
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/MoviesController.php',
+        return $this->render('index.html.twig', [
+            'title' => 'Avengers: Endgame'
         ]);
     }
 }
